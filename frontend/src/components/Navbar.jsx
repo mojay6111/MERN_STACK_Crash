492d46dfee0350c3c4516572bfce5b77ff1dd22e
CapMojay <edwingeorge521@gmail.com>
2026-01-22 12:34:10 +0300

    Add Zustand for state management; implement product creation and update UI components with Chakra UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,26 @@
-import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
-import { PlusSquareIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Container maxW="1140px">
+    <Container maxW={"1140px"} px={4}>
       <Flex
         h={16}
-        alignment={"center"}
-        justifyContent="space-between"
+        alignItems={"center"}
+        justifyContent={"space-between"}
         flexDir={{
           base: "column",
           sm: "row",
@@ -15,22 +28,27 @@ const Navbar = () => {
       >
         <Text
           fontSize={{ base: "22", sm: "28" }}
-          fontSizeWeight={"bold"}
+          fontWeight={"bold"}
           textTransform={"uppercase"}
           textAlign={"center"}
-          bigGradient="linear(to-r, cyan.400, blue.500)"
-          bgClip="text"
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
         >
-          <Link to={"/"}>Product Store</Link>
+          <Link to={"/"}>Product Store 🛒</Link>
         </Text>
+
         <HStack spacing={2} alignItems={"center"}>
           <Link to={"/create"}>
-            <Button leftIcon={<PlusSquareIcon />}>Create</Button>
+            <Button>
+              <PlusSquareIcon fontSize={20} />
+            </Button>
           </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
+          </Button>
         </HStack>
       </Flex>
     </Container>
   );
 };
-
 export default Navbar;
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,11 +1,71 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Input,
+  Container,
+  Heading,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useProductStore } from "../stores/productStore";
 
 const CreatePage = () => {
+  const { newProduct, setNewProduct } = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const { createProduct } = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    console.log("Success:", success);
+    console.log("Message:", message);
+  };
+
   return (
-    <div>
-      <h1>Create Product</h1>
-      <p>Form coming soon.</p>
-    </div>
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack>
+            <Input>
+              placeholder="Product Name" name="name" value={newProduct.name}
+              onChange=
+              {(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            </Input>
+
+            <Input>
+              placeholder="Price" name="price" type="number" value=
+              {newProduct.price}
+              onChange=
+              {(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+            </Input>
+
+            <Input>
+              placeholder="Image URL" name="image" value={newProduct.image}
+              onChange=
+              {(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+            </Input>
+
+            <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+              Add Product{" "}
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
   );
 };
 
